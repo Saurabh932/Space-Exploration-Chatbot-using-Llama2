ceb8893ceea31b81abbee01208bfc69309fb82b4
Saurabh Mandhalkar <saurabhmandhalkar03@gmail.com>
2024-01-17 23:45:39 +0530

    Complete Project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Space Chatbot',
+    version= '0.0.0',
+    author= 'Saurabh Mandhalkar',
+    author_email= 'saurabhmandhalkar03@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
