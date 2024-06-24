b9255ddd1d4bc547764b57b81b9cb43f4377ce67
Saurabh932 <saurabhmandhalkar03@gmail.com>
2024-06-24 23:43:25 +0530

    Updated

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from langchain.prompts import PromptTemplate
 from langchain_community.llms import CTransformers
 from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
-from src.prompt import *
 import os
+import boto3
 
 app = Flask(__name__)
 
@@ -15,6 +15,24 @@ load_dotenv()
 
 PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
 PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+S3_BUCKET_NAME = "spaceexplorationbucket"
+MODEL_NAME = "llama-2-7b-chat.ggmlv3.q4_0.bin"
+MODEL_LOCAL_PATH = f"model/{MODEL_NAME}"
+
+# Download model from S3 if it doesn't exist locally
+def download_model_from_s3(bucket_name, model_name, local_path):
+    if not os.path.exists(local_path):
+        s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+        s3.download_file(bucket_name, model_name, local_path)
+        print(f"Downloaded {model_name} from S3 to {local_path}")
+
+# Ensure the model directory exists
+os.makedirs(os.path.dirname(MODEL_LOCAL_PATH), exist_ok=True)
+
+# Download the model
+download_model_from_s3(S3_BUCKET_NAME, MODEL_NAME, MODEL_LOCAL_PATH)
 
 embeddings = download_hugging_face_embeddings()
 
@@ -28,21 +46,9 @@ docsearch = Pinecone.from_existing_index(index_name, embeddings)
 
 PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 
-chain_type_kwargs = {"prompt": PROMPT}
-
-# llm = CTransformers(
-#     model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
-#     model_type="llama",
-#     config={'max_new_tokens': 512, 'temperature': 0.8}
-# )
-
-
-# Specify the S3 URL of your model
-s3_model_url = "s3://spaceexplorationbucket/llama-2-7b-chat.ggmlv3.q4_0.bin"
-
-# Initialize CTransformers with the S3 URL
+# Initialize CTransformers with the local model path
 llm = CTransformers(
-    model=s3_model_url,
+    model=MODEL_LOCAL_PATH,
     model_type="llama",
     config={'max_new_tokens': 512, 'temperature': 0.8}
 )
